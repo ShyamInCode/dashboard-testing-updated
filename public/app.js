17bbc92aa81dc917c22e094feab4ea64046180d9
PATNALA SHYAM RAM <93208535+ShyamInCode@users.noreply.github.com>
2024-10-23 22:40:43 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginBtn = document.getElementById('loginBtn');
 
     loginBtn.addEventListener('click', () => {
-        window.location.href = `${process.env.LOGIN_URL}`;
+        window.location.href = 'https://dashboard-testing-updated.onrender.com/login';
     });
 
 });
